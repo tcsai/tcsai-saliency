2949e684db9ff4de5fb893035127c484b83fe9f8
kadar_akos <kadar.akos@gmail.com>
2017-02-03 16:50:23 +0100

    stop time

--- gazeanalyse.py
+++ gazeanalyse.py
@@ -1,31 +1,50 @@
+import sys
+sys.path.append("../PyGazeAnalyser")
 from pygazeanalyser import eyetribereader, gazeplotter
 import os
 
-IMGDIR = os.path.join('/home/akos/Documents/tcsai-saliency/imgs')
+IMGDIR = os.path.join('/home/akos/Documents/tcsai-saliency/mod_imgs')
 IMGNAMES = os.listdir(IMGDIR)
 outdir = os.path.join('/home/akos/Documents/tcsai-saliency/gazeviz')
 RESOLUTION = (1024, 768)
+DATAFILE = 'testdata/subject-5-1.tsv'
+# ,stop="stop_trial"
+gazedata = eyetribereader.read_eyetribe(DATAFILE,
+									    "start_trial",
+										stop_time=2995,  # first 3 seconds
+										missing=0.0)
 
-
-gazedata = eyetribereader.read_eyetribe('testdata/subject-0-1.tsv', 
-											     "start_trial", stop="stop_trial", 
-											      missing=0.0)
-
+imgs = open("imgorder.txt").read().split('\n')
 for i in range(len(gazedata)):
-	# Warm up cases
 	if i < 4:
-		imgname = gazedata[i]['events']['msg'][69][1].split()[-1]
-		imgname = imgname.split('.')[0] + '.jpg'
+		pass
+		# print gazedata[i]['events']['msg']
+		# imgname = gazedata[i]['events']['msg'][69][1].split()[-1]
+		# imgname = imgname.split('.')[0] + '.jpg'
 	else:
-		imgname = gazedata[i]['events']['msg'][71][1].split()[-1]
+		print i+4
+		print gazedata[i]['events']['msg']
+		# imgname = gazedata[i]['events']['msg'][71][1].split()[-1]
+		imgname = imgs[i+4]
 		imgname = imgname.split('.')[0] + '.jpg'
-		imgfile = os.path.join(IMGDIR,imgname)
+		imgfile = os.path.join(IMGDIR, imgname)
 		print imgname, imgfile
 		# get the fixations
 		fixations = gazedata[i]['events']['Efix']
-			# plot a heatmap
-		gazeplotter.draw_heatmap(fixations, RESOLUTION, imagefile=imgfile, durationweight=True,
-					alpha=0.5, savefilename=os.path.join(outdir,'%s_heatmap.png' % imgname))
-		print "YEEEEEEEEEEEE"
+		# get the saccades
+		saccades = gazedata[i]['events']['Esac']
+		# plot a heatmap
+		gazeplotter.draw_heatmap(fixations, RESOLUTION, imagefile=imgfile,
+					alpha=0.5, invert_x=True, invert_y=False,
+					savefilename=os.path.join(outdir, '%s_fix.png' % imgname))
 
-	
+		gazeplotter.draw_fixations(fixations, RESOLUTION, imagefile=imgfile,
+				alpha=0.5, invert_x=True, invert_y=False,
+				savefilename=os.path.join(outdir,'%s_heatmap.png' % imgname))
+
+
+		# gazeplotter.draw_scanpath(fixations, saccades, RESOLUTION, imagefile=imgfile,
+		#		alpha=0.5, invert_x=True, invert_y=False,
+		#		savefilename=os.path.join(outdir ,'%s_scan.png' % imgname))
+
+		print "YEEEEEEEEEEEE"
